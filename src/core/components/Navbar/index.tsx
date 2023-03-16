e3780508c61059b3df329bd0d5668c4a405b7589
Lucas Silva <lucas_segundinho@hotmail.com>
2023-03-16 22:50:07 +0000

    Navbar

---
+++ src/core/components/Navbar/index.tsx
@@ -0,0 +1,12 @@
+import './styles.scss';
+
+const Navbar = () => (
+    <nav className="main-nav">
+        <h4>
+            Bootcamp DevSuperior
+        </h4>
+    </nav>
+    
+);
+
+export default Navbar;
