68f7130bae7132eeb77fdea9177d55f1cf72d83d
Lucas Silva <lucas_segundinho@hotmail.com>
2023-03-20 00:06:41 +0000

    Format date

--- src/core/types/user.ts
+++ src/core/types/user.ts
@@ -1,10 +1,11 @@
 export type User = {
-    public_repos: number
-    followers: number
-    following: number
-    company: string
-    blog: string
-    location: string
-    created_at: string,
-    url: string
+    avatar_url: string;
+    public_repos: number;
+    followers: number;
+    following: number;
+    company: string;
+    blog: string;
+    location: string;
+    created_at: string;
+    html_url: string;
 }
--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Button from '../../core/components/Button';
 import { makeRequest } from '../../core/utils/request';
-import { ReactComponent as UserImage } from '../../core/assets/images/img-profile.svg';
 import './styles.scss';
 import { User } from '../../core/types/user';
+import InfoLoader from './components/Loaders/InfoLoader';
+import ImageLoader from './components/Loaders/ImageLoader';
+import dayjs from 'dayjs';
 
 type FormState = {
     user: string;
@@ -11,7 +13,8 @@ type FormState = {
 
 const Search = () => {
 
-    const[user, setUser] = useState<User>();
+    const[userInfo, setUserInfo] = useState<User>();
+    const [isLoading, setLoading] = useState(false);
     
     const [formData, setFormData] = useState<FormState>({
         user: ''
@@ -27,10 +30,16 @@ const Search = () => {
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
+        setLoading(true);
         const user = formData.user;
-
+        
         makeRequest({user: user})
-            .then(response => setUser(response.data));
+            .then(response => {
+                setUserInfo(response.data);
+            })
+            .finally(() => {
+                setLoading(false);
+        });
     }
 
     return (
@@ -49,34 +58,44 @@ const Search = () => {
                     <Button text="Encontrar" />
                 </form>
             </div>
-            {user === undefined || null ? <></> 
-            : (
+            {!isLoading ? <></> : (
                 <div className="page-user-card">
-                <div className="page-user-info">
-                    <div className="user-image">
-                        <UserImage />
-                    </div>
-                    <div className="row user-top-info"> 
-                        <span>Repositórios publicos: {user?.public_repos}</span>
-                        <span>Seguidores: {user?.followers}</span>
-                        <span>Seguindo: {user?.following}</span>
-                    </div>
-                    <div className="row user-info-card">
-                       <h1>Informações</h1>
-                        <div className="user-data">
-                            <span><strong>Empresa:</strong> {user?.company}</span>
-                            <span><strong>Website/Blog:</strong> {user.blog}</span>
-                            <span><strong>Localidade:</strong> {user.location}</span>
-                            <span><strong>Membro desde:</strong> {user.created_at}</span>
-                        </div>    
-                    </div>
-                    <div className="btn-details">
-                        <Button text="Ver perfil"/>
+                    <div className="page-user-info">
+                        <ImageLoader />
+                        <InfoLoader />
                     </div>
                 </div>
-            </div>
             )}
-           
+            {userInfo === undefined ? <></> : (
+                <div className="page-user-card">
+                    <div className="page-user-info">
+                        <div className="user-image">
+                            <img src={userInfo?.avatar_url} alt="user-img"/>
+                        </div>
+                        <div className="row user-top-info"> 
+                            <span>Repositórios publicos: {userInfo?.public_repos}</span>
+                            <span>Seguidores: {userInfo?.followers}</span>
+                            <span>Seguindo: {userInfo?.following}</span>
+                        </div>
+                        <div className="row user-info-card">
+                            <h1>Informações</h1>
+                            <div className="user-data">
+                                <span><strong>Empresa:</strong> {userInfo?.company}</span>
+                                <span><strong>Website/Blog:</strong> {userInfo?.blog}</span>
+                                <span><strong>Localidade:</strong> {userInfo?.location}</span>
+                                <span><strong>Membro desde: </strong> 
+                                    {dayjs(userInfo?.created_at,"YYYY-MM-DDTh:mm:ssZ").format("DD/MM/YYYY")}
+                                </span>
+                            </div>    
+                        </div>
+                        <div className="btn-details">
+                            <a href={userInfo?.html_url}>
+                                <Button text="Ver perfil"/>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
